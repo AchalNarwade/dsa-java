1fa707b7212ea28e2189e6f55435d4ecb6ed5d4e
AchalNarwade <achalnarwade211@gmail.com>
2026-01-16 20:42:07 +0530

    Implement in-place Running Sum for 1D array

---
+++ array/Running-sum-of-1d-array.java
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/running-sum-of-1d-array/?envType=problem-list-v2&envId=array
+
+/* Time Complexity =O(n)
+   Space Complexity = O(1) */
+
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int sum = 0;
+        for(int i = 0;i<nums.length;i++){
+            sum += nums[i];
+            nums[i] = sum;
+        }
+        return nums;
+    }
+}
+
