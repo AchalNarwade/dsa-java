91a293033bf519e8f5f0f6d625447db849b1b1de
AchalNarwade <achalnarwade211@gmail.com>
2026-04-11 21:55:12 +0530

    LC 125: Valid Palindrome (Two Pointer)

---
+++ string/ValidPalindrome.java
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/valid-palindrome/?envType=problem-list-v2&envId=string
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^a-zA-Z0-9]","");
+        int i=0;
+        int j=s.length()-1;
+        while(i<=j){
+            if(s.charAt(i) == s.charAt(j)){
+                i++;
+                j--;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
