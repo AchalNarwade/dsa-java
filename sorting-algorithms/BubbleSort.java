3e788c4851fe8b97b56b4ad72b57bc73cdadd2eb
AchalNarwade <achalnarwade211@gmail.com>
2026-04-23 22:29:30 +0530

    Added optimized Bubble Sort using swap flag

---
+++ sorting-algorithms/BubbleSort.java
@@ -0,0 +1,36 @@
+public class BubbleSort{
+    public static void bubblesort(int[] arr){
+
+    int n = arr.length;
+    boolean isSwapped;
+
+    for(int i=0;i<n-1;i++){
+         isSwapped = false;
+         for(int j=0;j<n-1-i;j++){
+             if(arr[j]>arr[j+1]){
+                  int temp = arr[j];
+                  arr[j] = arr[j+1];
+                  arr[j+1] = temp;
+
+                  isSwapped = true;
+             }
+        }
+        if(!isSwapped){
+            break;
+        }
+    }
+}
+
+    public static void main(String[] args){
+
+        int[] arr = {2,1,5,4,3};
+
+        bubblesort(arr);
+   
+        System.out.println("Sorted Array: ");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+}
